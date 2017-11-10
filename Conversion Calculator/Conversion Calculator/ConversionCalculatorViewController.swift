5fe19fbe93c95ae01452d68be0160966bf3ade1b
Teddy <teddyivanov95@gmail.com>
2017-11-10 00:01:14 -0600

    Refactored UIAlertsActions to be more readable.

--- Conversion Calculator/Conversion Calculator/ConversionCalculatorViewController.swift
+++ Conversion Calculator/Conversion Calculator/ConversionCalculatorViewController.swift
@@ -33,22 +33,32 @@ class ConversionCalculatorViewController: UIViewController {
     
     @IBAction func converterIsTapped(_ sender: Any) {
         let alert = UIAlertController(title: "Choose converter", message: nil, preferredStyle: UIAlertControllerStyle.actionSheet)
-        alert.addAction(UIAlertAction(title: converters[0].label, style: UIAlertActionStyle.default, handler: { (alertAction) in
-                self.outputDisplay.text = self.converters[0].outputUnit
-                self.inputDisplay.text = self.converters[0].inputUnit
-        }))
-        alert.addAction(UIAlertAction(title: converters[1].label, style: UIAlertActionStyle.default, handler: { (alertAction) in
+        
+        let fahrenheitToCelcius = UIAlertAction(title: converters[0].label, style: UIAlertActionStyle.default) {
+            (_) in
+            self.outputDisplay.text = self.converters[0].outputUnit
+            self.inputDisplay.text = self.converters[0].inputUnit
+        }
+        let celciusToFahrenheit = UIAlertAction(title: converters[1].label, style: UIAlertActionStyle.default) {
+            (_) in
             self.outputDisplay.text = self.converters[1].outputUnit
             self.inputDisplay.text = self.converters[1].inputUnit
-        }))
-        alert.addAction(UIAlertAction(title: converters[2].label, style: UIAlertActionStyle.default, handler: { (alertAction) in
+        }
+        let milesToKilometers = UIAlertAction(title: converters[2].label, style: UIAlertActionStyle.default) {
+            (_) in
             self.outputDisplay.text = self.converters[2].outputUnit
             self.inputDisplay.text = self.converters[2].inputUnit
-        }))
-        alert.addAction(UIAlertAction(title: converters[3].label, style: UIAlertActionStyle.default, handler: { (alertAction) in
+        }
+        let kilometerToMiles = UIAlertAction(title: converters[3].label, style: UIAlertActionStyle.default) {
+            (_) in
             self.outputDisplay.text = self.converters[3].outputUnit
             self.inputDisplay.text = self.converters[3].inputUnit
-            }))
+        }
+        
+        alert.addAction(fahrenheitToCelcius)
+        alert.addAction(celciusToFahrenheit)
+        alert.addAction(milesToKilometers)
+        alert.addAction(kilometerToMiles)
         
         self.present(alert, animated: true, completion: nil)
         }
