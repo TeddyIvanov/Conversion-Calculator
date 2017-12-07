104ae6cd56d53167443acf3cafa16e7f8e93c5b3
Teddy <teddyivanov95@gmail.com>
2017-12-07 16:08:56 -0600

    Implemented Conversion Calculator to calculate and refactored code.

--- Conversion Calculator/Conversion Calculator/ConversionCalculatorViewController.swift
+++ Conversion Calculator/Conversion Calculator/ConversionCalculatorViewController.swift
@@ -12,7 +12,11 @@ class ConversionCalculatorViewController: UIViewController {
     @IBOutlet weak var outputDisplay: UITextField!
     @IBOutlet weak var inputDisplay: UITextField!
     
+    var inputText = ""
+    var outputText = ""
+    var isPositive = true
     var converters = [Converter]()
+    var currentConverter = Converter(label: "fahrenheit to celcius", inputUnit: "°F", outputUnit: "°C")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,8 +26,8 @@ class ConversionCalculatorViewController: UIViewController {
         converters.append(Converter(label: "miles to kilometers", inputUnit: "mi", outputUnit: "km"))
         converters.append(Converter(label: "kilometers to miles", inputUnit: "km", outputUnit: "mi"))
         // Do any additional setup after loading the view.
-        outputDisplay.text = converters[0].outputUnit
-        inputDisplay.text = converters[0].inputUnit
+        outputDisplay.text = currentConverter.outputUnit
+        inputDisplay.text = currentConverter.inputUnit
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,45 +35,79 @@ class ConversionCalculatorViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //Handles the UIAlertController to change.
     @IBAction func converterIsTapped(_ sender: Any) {
         let alert = UIAlertController(title: "Choose converter", message: nil, preferredStyle: UIAlertControllerStyle.actionSheet)
         
-        let fahrenheitToCelcius = UIAlertAction(title: converters[0].label, style: UIAlertActionStyle.default) {
-            (_) in
-            self.outputDisplay.text = self.converters[0].outputUnit
-            self.inputDisplay.text = self.converters[0].inputUnit
+        for converter in converters {
+            alert.addAction(UIAlertAction(title: converter.label, style: UIAlertActionStyle.default) {
+                (_) in
+                self.outputDisplay.text = converter.outputUnit
+                self.inputDisplay.text = self.inputText + " " + converter.inputUnit
+                self.currentConverter.label = converter.label
+                self.currentConverter.outputUnit = converter.outputUnit
+                self.currentConverter.inputUnit = converter.inputUnit
+                if let input = Double(self.inputText){
+                    self.outputDisplay.text = self.determineCalculation(input: input, currentConverter: self.currentConverter)
+                }
+            })
         }
-        let celciusToFahrenheit = UIAlertAction(title: converters[1].label, style: UIAlertActionStyle.default) {
-            (_) in
-            self.outputDisplay.text = self.converters[1].outputUnit
-            self.inputDisplay.text = self.converters[1].inputUnit
-        }
-        let milesToKilometers = UIAlertAction(title: converters[2].label, style: UIAlertActionStyle.default) {
-            (_) in
-            self.outputDisplay.text = self.converters[2].outputUnit
-            self.inputDisplay.text = self.converters[2].inputUnit
-        }
-        let kilometerToMiles = UIAlertAction(title: converters[3].label, style: UIAlertActionStyle.default) {
-            (_) in
-            self.outputDisplay.text = self.converters[3].outputUnit
-            self.inputDisplay.text = self.converters[3].inputUnit
-        }
-        
-        alert.addAction(fahrenheitToCelcius)
-        alert.addAction(celciusToFahrenheit)
-        alert.addAction(milesToKilometers)
-        alert.addAction(kilometerToMiles)
-        
+            
         self.present(alert, animated: true, completion: nil)
-        }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
-
+    
+    @IBAction func clearButton(_ sender: UIButton) {
+        inputDisplay.text = "" + self.currentConverter.inputUnit
+        outputDisplay.text = "" + self.currentConverter.outputUnit
+        inputText = ""
+        isPositive = true
+    }
+    
+    @IBAction func flipSignButton(_ sender: UIButton) {
+        if isPositive {
+            inputText = "-" + inputText
+            isPositive = false
+        }else {
+            inputText.removeFirst()
+            isPositive = true
+        }
+        if let input = Double(inputText){
+             self.inputDisplay.text = inputText + " " + currentConverter.inputUnit
+            self.outputDisplay.text = determineCalculation(input: input, currentConverter: currentConverter)
+        }
+    }
+    
+    @IBAction func calculatorButtonTapped(sender: UIButton) {
+        inputText.append(sender.currentTitle!)
+        inputDisplay.text = inputText + " " + currentConverter.inputUnit
+        if let input = Double(inputText){
+            self.outputDisplay.text = determineCalculation(input: input, currentConverter: currentConverter)
+        }
+    }
+    
+    func determineCalculation(input: Double, currentConverter: Converter) -> String{
+        var output = 0.0
+        switch(currentConverter.label){
+            case "fahrenheit to celcius":
+                //FahrenheitToCelcius T(°C) = (T(°F) - 32) × 5/9
+                output = ((input - 32) * (5/9))
+                break
+            case "celcius to fahrenheit":
+                //celciusToFahrenheit T(°F) = T(°C) × 9/5 + 32
+                output = (input * (9/5)) + 32
+                break
+            case "miles to kilometers":
+                 //milesToKilometers 1 mile * 1.609344 = km
+                output = input * 1.609344
+                break
+            case "kilometers to miles":
+                //kmTomiles 1 km * 0.6214 mi = mile
+                output = input * 0.6214
+                break
+            default:
+                break
+        }
+        outputText = String.localizedStringWithFormat("%.2f", output) + " " + currentConverter.outputUnit
+        return outputText
+    }
 }
